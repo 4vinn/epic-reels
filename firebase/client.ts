d946b75db6dd6faf7b3721178402933ba1884bcf
4vinn <vineet.iitdelhi@gmail.com>
2025-04-15 23:56:11 +0530

    implmented firebase auth and firestore

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,25 +1,25 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp, getApp, getApps } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCjpgdR5qRKOuggdIj4jJrokemfUuQb20E",
-//   authDomain: "preptalk-92a7a.firebaseapp.com",
-//   projectId: "preptalk-92a7a",
-//   storageBucket: "preptalk-92a7a.firebasestorage.app",
-//   messagingSenderId: "493962333137",
-//   appId: "1:493962333137:web:650569669597baf196eef0",
-//   measurementId: "G-QSBZ4KJYNR",
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjpgdR5qRKOuggdIj4jJrokemfUuQb20E",
+  authDomain: "preptalk-92a7a.firebaseapp.com",
+  projectId: "preptalk-92a7a",
+  storageBucket: "preptalk-92a7a.firebasestorage.app",
+  messagingSenderId: "493962333137",
+  appId: "1:493962333137:web:650569669597baf196eef0",
+  measurementId: "G-QSBZ4KJYNR",
+};
 
-// // Initialize Firebase
-// const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
